baae73821c4d95d30454fb97311c5f5c50c6c43e
Tomsy Paul <tomsypaul@gmail.com>
2025-07-01 14:39:27 +0530

    hosts just 4..modifying close-all-terminals

--- close-all-terminals.sh
+++ close-all-terminals.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for i in `ps -A | grep bash | tail -n+2 | tr -s " " | cut -d" " -f2`
+for i in `ps -A | grep bash | tail -n 4 | tr -s " " | cut -d" " -f2`
 do
-kill "$i"
+kill -9 "$i"
 done
