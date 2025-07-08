b146a2e23432d64c97918612077906b29a155914
Tomsy Paul <tomsypaul@gmail.com>
2025-07-08 14:23:35 +0530

    close-all-terminals modified for test.sh

--- close-all-terminals.sh
+++ close-all-terminals.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-for i in `ps -A | grep bash | tail -n +2 | tr -s " " | cut -d" " -f2`
+size=`cat hostips | wc -l`
+for i in `ps -A | grep bash | tail -n +3 | tr -s " " | cut -d" " -f2`
 do
 kill -9 "$i"
 done
