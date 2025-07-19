051774177a6587058e448e6f9c2a760ca8b968d3
Tomsy Paul <tomsypaul@gmail.com>
2025-07-19 14:17:54 +0530

    Copied functions from alex

--- aciq_encr_decr_ha_single.py
+++ aciq_encr_decr_ha_single.py
@@ -15,6 +15,10 @@ import logging
 import time
 from hashlib import sha256
 
+from joblib import Parallel, delayed
+import multiprocessing
+N_JOBS = multiprocessing.cpu_count()
+
 from math import ceil
 from random import Random
 from torch.autograd import Variable
@@ -22,6 +26,10 @@ from torchvision import datasets, transforms
 
 from functools import reduce
 from ftl.encryption import paillier, encryption
+
+def clip_gradients(grads, min_v, max_v):
+    results = [torch.clip(t, min=min_v, max=max_v).numpy() for t in grads]
+    return results
 
 def do_sum(x1, x2):
     results = []
@@ -33,6 +41,69 @@ def aggregate_gradients(gradient_list, weight=0.5):
     results = reduce(do_sum, gradient_list)
     return results
 
+def aggregate_losses(loss_list):
+    return np.sum(loss_list)
+
+
+def batch_enc_per_layer(publickey, party, r_maxs, bit_width=16, batch_size=100):
+    result = []
+    og_shapes = []
+
+    for layer, r_max in zip(party, r_maxs):
+        enc, shape_ = encryption.encrypt_matrix_batch(publickey, layer, batch_size=batch_size, bit_width=bit_width,
+                                                      r_max=r_max)
+        result.append(enc)
+        og_shapes.append(shape_)
+    return result, og_shapes
+
+
+def batch_dec_per_layer(privatekey, party, og_shapes, r_maxs, bit_width=16, batch_size=100):
+    result = []
+    for layer, r_max, og_shape in zip(party, r_maxs, og_shapes):
+        result.append(
+            encryption.decrypt_matrix_batch(privatekey, layer, og_shape, batch_size=batch_size, bit_width=bit_width,
+                                            r_max=r_max).astype(np.float32))
+    return result
+
+
+def quantize(party, bit_width=16, r_max=0.5):
+    result = []
+    for component in party:
+        x, _ = encryption.quantize_matrix(component, bit_width=bit_width, r_max=r_max)
+        result.append(x)
+    return np.array(result)
+
+
+def quantize_per_layer(party, r_maxs, bit_width=16):
+    # result = []
+    # for component, r_max in zip(party, r_maxs):
+    #     x, _ = encryption.quantize_matrix_stochastic(component, bit_width=bit_width, r_max=r_max)
+    #     result.append(x)
+    result = Parallel(n_jobs=N_JOBS)(
+        delayed(encryption.quantize_matrix_stochastic)(component, bit_width=bit_width, r_max=r_max) for component, r_max
+        in zip(party, r_maxs))
+    result = np.array(result)[:, 0]
+    return result
+
+
+def unquantize(party, bit_width=16, r_max=0.5):
+    result = []
+    for component in party:
+        result.append(encryption.unquantize_matrix(component, bit_width=bit_width, r_max=r_max).astype(np.float32))
+    return np.array(result)
+
+
+def unquantize_per_layer(party, r_maxs, bit_width=16):
+    # result = []
+    # for component, r_max in zip(party, r_maxs):
+    #     result.append(encryption.unquantize_matrix(component, bit_width=bit_width, r_max=r_max).astype(np.float32))
+    result = Parallel(n_jobs=N_JOBS)(
+        delayed(encryption.unquantize_matrix)(component, bit_width=bit_width, r_max=r_max) for component, r_max
+        in zip(party, r_maxs)
+    )
+    return np.array(result)
+
+
 if  __name__ == "__main__":
     publickey=paillier.PaillierPublicKey(27236700922646976555595848507913589494886491119135730116014077185311243444450372255376489388880173022641848729747213088746475118480344996406749938547224285029951417411158327330610634671230458266993515963753271442282969744291116368707834837036890519842176076657317424175485854349519237230877898852294685281803161775833139254050216610420167131637216465657783550454961204111753470621658424459969937833601118914414496472033175054121693273513687334787107976849759736841476647931918984474457173711208172669939800415050356154977238127550304510079658979903408556459392897794799075517038480041829170731623511642064703877042081)
     privatekey=paillier.PaillierPrivateKey(publickey,151731466574845229173039099025495922095008727037737139342593172671294559941585338353476978224816381692776568780654141379462769869960946855764863957672293447968060874024631012137102252157537274255768018381630660935684213060267736952413889789815316743297286676968944107787802792481731771876027538148701189447527,179505949144779488794639217468096838709557457111534772428337237788171867936597162601187903575540819127792895820899862337184627927909110508331699702369829930039159510893967892328575714125966954175352601542034250713387089395862180071677366675266103958356045025432474981187150877764707656153946356889833385368503)
@@ -40,32 +111,49 @@ if  __name__ == "__main__":
     x = torch.tensor([[1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16], [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]], dtype=torch.float32)
     npx=x.numpy()
     cipherx=encryption.encrypt_matrix(publickey, npx)
-    #serialized_cipherx = pickle.dumps(cipherx)
-    #tensor_to_send = torch.ByteTensor(list(serialized_cipherx))
-    #dist.send(tensor=tensor_to_send,dst=1)
-    #bytes_sent += len(tensor_to_send)
-    #bytes_sent += tensor_to_send.nelement() * tensor_to_send.element_size()
-    #print("In 0.. Sent..x=",x,"\nCipherx=",cipherx,"\nBytes Sent..",bytes_sent)
     y = torch.tensor([[17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32], [25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]], dtype=torch.float32)
     npy=y.numpy()
     ciphery=encryption.encrypt_matrix(publickey, npy)
-    #serialized_ciphery = pickle.dumps(ciphery)
-    #byte_tensor_y=torch.ByteTensor(list(serialized_ciphery))
-    #received_tensor=copy.deepcopy(byte_tensor_y)
-    #received_tensor=copy.deepcopy(tensor_to_send)
-    #dist.recv(tensor=received_tensor,src=0)
-    
-    #received_bytes = bytes(received_tensor.tolist())
-    #cipherz = pickle.loads(received_bytes)
-    #print("In 1.. Recvd..",cipherz)
-    #zarray=np.array([encryption.decrypt_matrix(privatekey, item).astype(np.float32) for item in cipherz])    
-    #z=torch.from_numpy(zarray)
-    #print("z=",z)
     test_list=[]
     test_list.append(cipherx)
     test_list.append(ciphery)
     cipherresult=aggregate_gradients(test_list)
     result=np.array([encryption.decrypt_matrix(privatekey, item).astype(np.float32) for item in cipherresult])
+    print("\nx array = ",x)
+    print("\ny array = ",y)
     print("\nSum array = ",result)
     resulttensor=torch.from_numpy(result)
     #print("\nSum tensor = ",resulttensor)
+    num_clients=2
+    grads_batch_clients=[x,y]
+    q_width=16
+    sizes = [item.size * num_clients for item in grads_batch_clients[0]]
+    max_values = []
+    min_values = []
+    for layer_idx in range(len(grads_batch_clients[0])):
+        max_values.append([np.max([item[layer_idx] for item in grads_batch_clients])])
+        min_values.append([np.min([item[layer_idx] for item in grads_batch_clients])])
+    grads_max_min = np.concatenate([np.array(max_values),np.array(min_values)],axis=1)
+    clipping_thresholds = encryption.calculate_clip_threshold_aciq_g(grads_max_min, sizes, bit_width=q_width)
+
+
+    r_maxs = [x * num_clients for x in clipping_thresholds]
+
+    # grads_0 = encryption.clip_with_threshold(grads_0, clipping_thresholds)
+    # grads_1 = encryption.clip_with_threshold(grads_1, clipping_thresholds)
+    grads_batch_clients = [encryption.clip_with_threshold(item, clipping_thresholds)
+                            for item in grads_batch_clients]
+
+    # grads_0 = quantize_per_layer(grads_0, r_maxs, bit_width=q_width)
+    # grads_1 = quantize_per_layer(grads_1, r_maxs, bit_width=q_width)
+    grads_batch_clients = [quantize_per_layer(item, r_maxs, bit_width=q_width)
+                            for item in grads_batch_clients]
+
+    # grads = aggregate_gradients([grads_0, grads_1])
+    # loss_value = aggregate_losses([0.5 * loss_value_0, 0.5 * loss_value_1])
+    grads = aggregate_gradients(grads_batch_clients)
+    client_weight = 1.0 / args.num_clients
+    loss_value = aggregate_losses([item * client_weight for item in loss_batch_clients])
+
+    # grads = unquantize_per_layer(grads, r_maxs, bit_width=q_width)
+    grads = unquantize_per_layer(grads, r_maxs, bit_width=args.q_width)
