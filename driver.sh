ce625d51628b4db1b50f0d0188d3602da395dab1
Tomsy Paul <tomsypaul@gmail.com>
2025-08-08 14:15:02 +0530

    run-cifar.py modified to run.py in driver

--- driver.sh
+++ driver.sh
@@ -47,7 +47,7 @@ echo layout-up >> files-to-upload
 echo layout-down >> files-to-upload
 #echo secrets >> files-to-upload
 echo keys >> files-to-upload
-echo run-cifar.py >> files-to-upload
+echo run.py >> files-to-upload
 echo partition_sizes >> files-to-upload
 
 #rest of the process
